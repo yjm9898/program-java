4511c587708f68c651f7f67aec72a3b9c2f95862
悬崖的鹰 <qtsyouxiang@163.com>
2016-12-27 23:33:02 +0800

    update a file

--- dubboTest/SayService/src/test/java/Test.java
+++ dubboTest/SayService/src/test/java/Test.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.WeakHashMap;
 
 /**
  * @author <a href="qiaotaosheng@wxchina.com">TaoSheng.Qiao</a>
@@ -9,44 +6,5 @@ import java.util.WeakHashMap;
  * @date 2016-10-10 14:59
  */
 public class Test {
-    public static void main(String[] args) {
-        String s = "e DFGDF df fdf gg    fdgf";
-//        new Test().splite(s);
-        new Test().ref();
-    }
-
-
-    private void splite(String s){
-        String[] strArray = s.split("\\s+");
-        for (String str : strArray){
-            System.out.println(str);
-        }
-    }
-
-    private void switchFun(String s){
-        switch (s){
-            case "E":
-                System.out.println("good");
-                break;
-            case "w":
-                System.out.println("yes");
-                break;
-            default:
-                System.out.println("default");
-        }
-    }
-
-    private void ref(){
-        WeakHashMap weakHashMap = new WeakHashMap();
-
-        List<WeakHashMap<byte[][], byte[][]>> maps = new ArrayList<WeakHashMap<byte[][], byte[][]>>();
-
-        for (int i = 0; i < 1000; i++) {
-            WeakHashMap<byte[][], byte[][]> d = new WeakHashMap<byte[][], byte[][]>();
-            d.put(new byte[1000][1000], new byte[1000][1000]);
-            maps.add(d);
-            System.gc();
-            System.err.println(i);
-        }
-    }
+    
 }

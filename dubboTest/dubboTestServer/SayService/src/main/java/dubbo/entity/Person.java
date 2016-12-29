98938df5a9569cc59f08c688442287e8b04b4fef
悬崖的鹰 <qtsyouxiang@163.com>
2016-12-29 21:13:21 +0800

    分离服务和调用者，并修改日志

---
+++ dubboTest/dubboTestServer/SayService/src/main/java/dubbo/entity/Person.java
@@ -0,0 +1,36 @@
+package dubbo.entity;
+
+/**
+ * @author <a href="qiaotaosheng@wxchina.com">TaoSheng.Qiao</a>
+ * @version 1.0.0
+ * @description:
+ * @date 2016-12-28 15:45
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
